8046aab5350962f025b4813c3ae9b0ec728e4d3b
Stefano Dal Prà <Stefano Dal Pr>
2026-01-26 13:22:23 +0000

    using only Symfony Translator

--- src/Plugins/TranslatorInterface.php
+++ src/Plugins/TranslatorInterface.php
@@ -2,11 +2,9 @@
 
 namespace DalPraS\SmartTemplate\Plugins;
 
-use Laminas\Translator\TranslatorInterface as LaminasTranslatorInterface;
-use Laminas\Validator\Translator\TranslatorInterface as LaminasOldTranslatorInterface;
 use Symfony\Contracts\Translation\TranslatorInterface as SymfonyTranslatorInterface;
 
-interface TranslatorInterface extends LaminasTranslatorInterface, SymfonyTranslatorInterface, LaminasOldTranslatorInterface
+interface TranslatorInterface extends SymfonyTranslatorInterface
 {
     
 }
