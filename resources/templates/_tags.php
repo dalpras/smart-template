defe8a06dc3af582aa7aca8f7c40781f50fdb89b
Stefano Dal Prà <Stefano Dal Pr>
2026-05-19 12:41:13 +0000

    add new tags

--- resources/templates/_tags.php
+++ resources/templates/_tags.php
@@ -1,25 +1,87 @@
 <?php
 
 return [
-    'div' => '<div {attributes}>{content}</div>',
+    'h1' => <<<HTML
+        <h1 {attributes}>{content}</h1>
+        HTML,
 
-    'span' => '<span {attributes}>{content}</span>',
+    'h2' => <<<HTML
+        <h2 {attributes}>{content}</h2>
+        HTML,
 
-    'p' => '<p {attributes}>{content}</p>',
+    'h3' => <<<HTML
+        <h3 {attributes}>{content}</h3>
+        HTML,
 
-    'h1' => '<h1 {attributes}>{content}</h1>',
+    'h4' => <<<HTML
+        <h4 {attributes}>{content}</h4>
+        HTML,
 
-    'h2' => '<h2 {attributes}>{content}</h2>',
+    'span' => <<<HTML
+        <span {attributes}>{content}</span>
+        HTML,
 
-    'h3' => '<h3 {attributes}>{content}</h3>',
+    'section' => <<<HTML
+        <section {attributes}>{content}</section>
+        HTML,
 
-    'a' => '<a href="{href}" {attributes}>{content}</a>',
+    'strong' => <<<HTML
+        <strong {attributes}>{content}</strong>
+        HTML,
 
-    'button' => '<button type="{type}" {attributes}>{content}</button>',
+    'sup' => <<<HTML
+        <sup>{content}</sup>
+        HTML,
 
-    'ul' => '<ul {attributes}>{content}</ul>',
+    'p' => <<<HTML
+        <p {attributes}>{content}</p>
+        HTML,
 
-    'ol' => '<ol {attributes}>{content}</ol>',
+    'dl' => <<<HTML
+        <dl {attributes}>{content}</dl>
+        HTML,
 
-    'li' => '<li {attributes}>{content}</li>',
+    'dt' => <<<HTML
+        <dt {attributes}>{text}</dt>
+        HTML,
+
+    'dd' => <<<HTML
+        <dd {attributes}>{text}</dd>
+        HTML,        
+
+    'picture' => <<<HTML
+        <picture>{content}</picture>
+        HTML,
+
+    'source' => <<<HTML
+        <source srcset="{src}" media="{media}">
+        HTML,       
+
+    'ul' => <<<HTML
+        <ul {attributes}>{content}</ul>
+        HTML,
+
+    'ol' => <<<HTML
+        <ul {attributes}>{content}</ul>
+        HTML,
+
+    'li' => <<<HTML
+        <li {attributes}>{content}</li>
+        HTML,
+
+    'div' => <<<HTML
+        <div {attributes}>{content}</div>
+        HTML,
+
+    'input' => <<<HTML
+        <input type="{type}" value="{value}" {attributes} />
+        HTML,        
+
+    'a' => <<<HTML
+        <a aria-label="{label}" href="{url}" {attributes}>{content}</a>
+        HTML,
+
+    'img' => <<<HTML
+        <img src="{src}" alt="{alt}" {attributes} />
+        HTML,
 ];
